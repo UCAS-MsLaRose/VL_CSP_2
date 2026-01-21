86b554fcd4448cf1e7ceb167d80faabcfcfbaf18
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2026-01-21 13:43:33 -0700

    variables notes

--- notes/variables.py
+++ notes/variables.py
@@ -1,3 +1,16 @@
 name = "Xavier"
 
+money = -9999999999999
+
+room = 201
+
+pi = 3.14
+
+pie = input("What is your favorite kind of pie: ")
+
+time = "1:25 PM"
+
 print(name)
+print(room)
+print("Programming is the best!")
+print(time, "is", pie, "pie time!")
