0bc8eeaf4bce2c435ff87a9df9df9ed9ecc2bf36
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2026-01-21 14:41:37 -0700

    variables notes

--- notes/variables.py
+++ notes/variables.py
@@ -1,3 +1,15 @@
 name = "Treyson"
 
+room = 201
+
+subject = "Computer Science"
+
+pi = 3.14
+
+money = -99999999999999999999.99
+
 print(name)
+
+print("Computer science is the best!")
+
+print("Ms. LaRose teaches", subject, "in room", room)
