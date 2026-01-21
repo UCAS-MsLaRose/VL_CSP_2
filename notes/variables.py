7517529c87a484f84d120d48e7b5e62fc5f07d9e
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2026-01-21 14:25:26 -0700

    first commit

--- notes/variables.py
+++ notes/variables.py
@@ -1,16 +1,3 @@
-name = "Xavier"
-
-money = -9999999999999
-
-room = 201
-
-pi = 3.14
-
-pie = input("What is your favorite kind of pie: ")
-
-time = "1:25 PM"
+name = "Treyson"
 
 print(name)
-print(room)
-print("Programming is the best!")
-print(time, "is", pie, "pie time!")
