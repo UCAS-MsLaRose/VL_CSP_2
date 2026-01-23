4ff5af088fac870161f6614944846089d34a483b
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2026-01-23 14:13:08 -0700

    notes with 6th

---
+++ 6th/notes/integers_floats.py
@@ -0,0 +1,13 @@
+"""price = float(input("How much did it cost: $"))
+
+tax_rate = float(input("What percent is the tax: %"))/100
+
+total_price = price * (1 + tax_rate)
+
+print(round(total_price, 2))"""
+
+apples = int(input("How many apples are there: "))
+
+friends = int(70.33)
+
+print("Each friend gets", apples//friends, "and I have", apples%friends, "left over")
