a699476e8dcb2e8ec48586c434fc206ee5a6fec8
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2026-01-27 11:50:30 -0700

    3rd notes

---
+++ 3rd/notes/strings.py
@@ -0,0 +1,22 @@
+"""color = input("What is your favorite color: ").strip().title()
+
+print(color, "is a really cool color!")"""
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+sentence = "The quick brown fox jumps over the lazy dog."
+alphabet.strip()
+
+"""print(alphabet[4:8])
+word = input("What word do you want to change: ")
+new = input("What will the new word be: ")
+start =sentence.find(word)
+print("("+sentence[start:start+len(word)]+")")
+print(sentence)
+sentence = sentence.replace(word, new)
+print(sentence)"""
+
+number = int(input("Tell me a number: "))
+print(number/5)
+
+room = "Ms. LaRose's room"
+quote = 'Treyson said "Yolo"'
