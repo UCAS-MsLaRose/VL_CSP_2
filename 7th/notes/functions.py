f54cab6fffabc0daab27128c8402b7895a030bdc
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2026-02-04 14:42:31 -0700

    funcitons

---
+++ 7th/notes/functions.py
@@ -0,0 +1,28 @@
+def hello():
+    print("Hello World!")
+
+def full_name(first, last):
+    hello()
+    return f"{first} {last}"
+
+hello()
+print(full_name("Vienna", "LaRose"))
+name = full_name("Tia", "LaRose")
+print(f"{name} doesn't like {full_name('Annabeth', 'Chase')}")
+
+
+def factoral(number):
+    total = 1
+    for i in range(1, number +1):
+        total *= i
+    return total
+
+for num in range(1,10):
+    print(f"The factoral of {num} is {factoral(num)}")
+
+name = "Ms. LaRose"
+
+def admin():
+    print(f"{name} is the admin for the lab!")
+
+admin()
