1ef7b0ce05857618f9f3503b393babd2ffe05b8b
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2026-02-18 11:50:37 -0700

    Update for C programming

---
+++ 3rd/C/notes/variables.c
@@ -0,0 +1,11 @@
+// This is a comment in C 
+#include <stdio.h> // lets us create inputs and outputs
+
+int main(){
+    int number = 12;
+    float pi = 3.14;
+
+    printf("We have the number %d\n", number); // \n tells the computer to move to the next line
+    printf("%f\n", pi);
+    return 0; //always the last line of main
+}
