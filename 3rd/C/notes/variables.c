3ea540ebc399159d44a0fabdb9e8dc64b3785e27
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2026-02-19 11:50:28 -0700

    finished notes

--- 3rd/C/notes/variables.c
+++ 3rd/C/notes/variables.c
@@ -3,9 +3,20 @@
 
 int main(){
     int number = 12;
-    float pi = 3.14;
+    const float pi = 3.14;
+    char name[] = "Xavier";
+    char person[50];
+    int age;
 
+    printf("Tell me your name\n");
+    scanf("%s", &person);
+
+    printf("Tell me how old you are\n");
+    scanf("%d", &age);
+
+    printf("Name is %s and age is %d\n", person, age);
     printf("We have the number %d\n", number); // \n tells the computer to move to the next line
     printf("%f\n", pi);
+    printf("%s is %d years old\n", name, age);
     return 0; //always the last line of main
 }
